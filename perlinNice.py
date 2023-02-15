a2da6d6f1430f3dc185c7557143d7a5d9baeba4b
SilverFishSeeker <silverfishseeker@gmail.com>
2023-02-15 12:48:53 +0100

    perlin nice, separar el grower

---
+++ perlinNice.py
@@ -0,0 +1,36 @@
+
+import random, numpy as np
+
+def subPerlin(size, step): # genera una frecuencia de ruidete con valores en el intervalo [0,1)
+    if step <= 0: raise ValueError("menor o igual que 0")
+    mapa = np.zeros([(size//step+1)*step+1]*2) # necesitamos hueco de más para los "pivotes" exteriores
+    for i in range(size//step+2):
+        for j in range(size//step+2):
+            mapa[i*step,j*step] = random.random()
+            if i > 0 and j > 0:
+                for i2 in range(step):
+                    for j2 in range(step):
+                        #Interpolación bilineal
+                        x2 = i  * step
+                        x1 = x2 - step
+                        y2 = j  * step
+                        y1 = y2 - step
+                        mapa[x1+i2,y1+j2] = (
+                            mapa[x1,y1]*(step-i2)*(step-j2)
+                            +mapa[x2,y1]*i2*(step-j2)
+                            +mapa[x1,y2]*(step-i2)*j2
+                            +mapa[x2,y2]*i2*j2
+                            )/step/step
+    return mapa[:size,:size]
+
+def perlinNoise(size, stepsWeights):
+    """
+    stepsWeights: lista de duplas de números. Cada dupla es: (número de steps, peso de esa frecuencia)
+    """
+    return np.asarray([subPerlin(size, x[0])*x[1] for x in stepsWeights], dtype=float).sum(axis=0)
+
+def perlinNoiseFreq(size, stepsFreqs):
+    """
+    Igual que perlinNoise pero por fecuencias en vez de steps
+    """
+    return perlinNoise(size, ((size//x[0], x[1]) for x in stepsFreqs)) 
--- tdi.py
+++ tdi.py
@@ -5,83 +5,8 @@ from scipy import ndimage
 import cv2 as cv # pip install opencv-python
 from perlin_numpy import generate_fractal_noise_2d # pip3 install git+https://github.com/pvigier/perlin-numpy
 
-def regionGrower(image, nregions, threshold, seed=random.random()):
-
-  def judge(p1, p2):
-    return abs(image[p1] - image[p2]) < threshold
-
-  class Region():
-    def __init__(s, seed, value, freespace):
-      s.free = freespace # pixeles disponibles
-      s.val = value # identificador
-      s.place = []
-      s.border = []
-      s.seed = seed
-      if s.isfree(seed):
-        s.addPoint(seed)
-      else:
-        raise ValueError
-
-    def isfree(s, point):
-      if point[0]<0 or point[1]<0: # indeces negativos son válidos en python
-        return False
-      try:
-        return s.free[point] == 0
-      except IndexError:
-        return False
-
-    def addPoint(s, point):
-      s.place.append(point)
-      s.free[point] = s.val
-      x, y = point
-      for i, j in [(-1,0),(1,0),(0,-1),(0,1)]:
-        new = x+i, y+j
-        if s.isfree(new) and judge((x,y), new):
-          s.border.append(new)
-
-    def __str__(s):
-      return str(s.val)+"|"+str(s.seed)
-
-    def grow(s):
-      while s.border:
-        candidate = s.border.pop(int(random.random()*len(s.border)))
-        if s.isfree(candidate):
-          s.addPoint(candidate)
-          return True
-      return False
-
-  random.seed(seed)
-  xlen, ylen = image.shape
-
-  freespace = np.zeros((xlen, ylen),dtype=int) # píxeles sin escoger
-  regions = []
-  closed = []
-  freenumber = xlen*ylen-nregions
-
-  for i in range(nregions):
-    while(True):
-      seed = int(random.random()*xlen), int(random.random()*ylen)
-      for r in regions:
-        if r.seed == seed:
-          break
-      else:
-        regions.append(Region(seed, i+1, freespace))
-        break
-
-  while freenumber > 0:
-    if len(regions) == 0:
-      regions = closed
-      closed = []
-      threshold*=1.1
-
-    curr = regions.pop(0)
-    if (curr.grow()):
-      regions.append(curr)
-      freenumber += -1
-    else:
-      closed.append(curr)
-
-  return freespace * 256 / nregions
+from regionGrower import regionGrower
+from perlinNice import *
 
 def generateMap(size, name):
   name = os.path.join("tests",name)
@@ -91,7 +16,11 @@ def generateMap(size, name):
     pass
 
   print("start")
-  arr = generate_fractal_noise_2d((size, size), (2,2), int(math.log(size,10)), 0.4)
+  #arr = generate_fractal_noise_2d((size, size), (2,2), int(math.log(size,10)), 0.4)
+  #arr = np.asarray( perlinNoise(size, size, [200,100,30,8],[50,30,19,1]))
+  #arr = np.asarray( perlinNoise(size, size, [(200,50),(100,30),(30,19),(8,1)]))
+  arr = perlinNoiseFreq(size, [(2,1)])
+
   # shape(alto, ancho),
   # res(number of peridos per axis),
   # octaves: number of octaves,
